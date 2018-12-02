da49d0b5d8cbbf05ab1d42670c01f1bc0bab077b
doc97 <daniel.riissanen@gmail.com>
2018-12-02 21:54:22 +0200

    Fix brackets and spacing in Tennis.java

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -27,23 +27,23 @@ public class TennisGame {
         String score = "";
         if (m_score1==m_score2) {
             return sameScore(m_score1);
-        } else if (m_score1>=4 || m_score2>=4)
-        {
+        } else if (m_score1>=4 || m_score2>=4) {
             int minusResult = m_score1-m_score2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
             else score ="Win for player2";
-        }
-        else
-        {
+        } else {
             int tempScore=0;
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+            for (int i=1; i<3; i++) {
+                if (i==1) {
+                    tempScore = m_score1;
+                } else {
+                    score += "-";
+                    tempScore = m_score2;
+                }
+
+                switch(tempScore) {
                     case 0:
                         score+="Love";
                         break;
