6fc359310850b90f70eaaab07115ca1fcf8f10de
doc97 <daniel.riissanen@gmail.com>
2018-12-02 21:59:40 +0200

    Move scoring situations into own methods

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -24,47 +24,59 @@ public class TennisGame {
     }
 
     public String getScore() {
-        String score = "";
         if (m_score1==m_score2) {
             return sameScore(m_score1);
         } else if (m_score1>=4 || m_score2>=4) {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            return tieScore(m_score1, m_score2);
         } else {
-            int tempScore=0;
-            for (int i=1; i<3; i++) {
-                if (i==1) {
-                    tempScore = m_score1;
-                } else {
-                    score += "-";
-                    tempScore = m_score2;
-                }
-
-                switch(tempScore) {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            return normalScore(m_score1, m_score2);
         }
-        return score;
     }
 
     private String sameScore(int score) {
         if (score < 0 || score >= SCORE_NAMES.length)
             return "Deuce";
         return SCORE_NAMES[score] + "-All";
+    }
+
+    private String tieScore(int score1, int score2) {
+        int minusResult = score1 - score2;
+        if (minusResult==1)
+            return "Advantage player1";
+        else if (minusResult ==-1)
+            return "Advantage player2";
+        else if (minusResult>=2)
+            return "Win for player1";
+        else
+            return "Win for player2";
+    }
+
+    private String normalScore(int score1, int score2) {
+        StringBuilder result = new StringBuilder();
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = score1;
+            } else {
+                result.append("-");
+                tempScore = score2;
+            }
+
+            switch(tempScore) {
+                case 0:
+                    result.append("Love");
+                    break;
+                case 1:
+                    result.append("Fifteen");
+                    break;
+                case 2:
+                    result.append("Thirty");
+                    break;
+                case 3:
+                    result.append("Forty");
+                    break;
+            }
+        }
+        return result.toString();
     }
 }
